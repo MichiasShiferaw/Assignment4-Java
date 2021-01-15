198f7978e68ba7de5d9aea70f112bd7c78474b48
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-14 23:59:47 -0500

    Update A4.java

--- A4.java
+++ A4.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List
+import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.InputStreamReader;
